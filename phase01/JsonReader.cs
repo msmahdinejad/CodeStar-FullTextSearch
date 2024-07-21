a90b77f90ad49dfe43b8db2510750dbbd8b1a31c
Mohammad Saleh Mahdinejad <msmahdinejad@gmail.com>
2024-07-21 18:19:50 +0330

    Some changes were made 6.

--- phase01/JsonReader.cs
+++ phase01/JsonReader.cs
@@ -1,4 +1,5 @@
-using System.Dynamic;
+using System.Reflection;
+using System.Resources;
 using System.Text.Json;
 
 namespace phase01;
@@ -7,8 +8,10 @@ public class JsonReader<T> : IDataReader<T>
 {
     public async Task<List<T>> Read()
     {
-        var path = GetJson();
-        var jsonData = await File.ReadAllTextAsync(path);
+        var listName = GetJson();
+        //var jsonData = await File.ReadAllTextAsync(path);
+        ResourceManager resourceManager = new ResourceManager($"phase01.{listName}", Assembly.GetExecutingAssembly());
+        string jsonData = resourceManager.GetString(listName);
         return JsonSerializer.Deserialize<List<T>>(jsonData);
     }
 
--- phase01/Program.cs
+++ phase01/Program.cs
@@ -4,14 +4,21 @@ public class Program
 {
     public static async Task Main()
     {
-        var topThreeStuden = await GetTopThreeStudent();
-        foreach (var student in topThreeStuden)
+        try
         {
-            Console.WriteLine(student.ToString());
+            var topThreeStuden = await GetTopStudent(3);
+            foreach (var student in topThreeStuden)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
 
-    public static async Task<IEnumerable<StudentGradeInfo>> GetTopThreeStudent()
+    public static async Task<IEnumerable<StudentGradeInfo>> GetTopStudent(int num)
     {
         var studentData = new JsonReader<Student>();
         var gradeData = new JsonReader<Grade>();
@@ -25,7 +32,7 @@ public class Program
                 FirstName = student.FirstName,
                 LastName = student.LastName,
                 Average = gradeList.Any() ? gradeList.Average(grade => grade.Score) : 0
-            }).OrderByDescending(student => student.Average).Take(3);
+            }).OrderByDescending(student => student.Average).Take(num);
 
         return topThreeStudent;
 
