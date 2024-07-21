f3147caf0e9e921209eb3ecb4ba91384dcdcf6d4
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2024-07-21 10:28:29 +0330

    Added JsonFile Functions and completed project.

--- phase01/Program.cs
+++ phase01/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text.Json;
+﻿using System.Text.Json;
 
 public class Student
 {
@@ -10,6 +8,14 @@ public class Student
     public string FirstName { get { return _firstName; } set { _firstName = value; } }
     private string _lastName;
     public string LastName { get { return _lastName; } set { _lastName = value; } }
+
+    public static List<Student> ReadStudentsFromJsonFile()
+    {
+        string path = "Students.txt";
+        string JsonData = File.ReadAllText(path);
+        List<Student> students = JsonSerializer.Deserialize<List<Student>>(JsonData);
+        return students;
+    }
 }
 
 public class Grade
@@ -20,12 +26,35 @@ public class Grade
     public string Lesson { get { return _lesson; } set { _lesson = value; } }
     private double _score;
     public double Score { get { return _score; } set { _score = value; } }
+
+    public static List<Grade> ReadGradesFromJsonFile()
+    {
+        string path = "Grades.txt";
+        string JsonData = File.ReadAllText(path);
+        List<Grade> grades = JsonSerializer.Deserialize<List<Grade>>(JsonData);
+        return grades;
+    }
 }
 
 public class Program
 {
     public static void Main()
     {
-        
+
+        var studentList = Student.ReadStudentsFromJsonFile().GroupJoin(Grade.ReadGradesFromJsonFile(),
+            student => student.StudentNumber,
+            grade => grade.StudentNumber,
+            (student, gradeList) => new
+            {
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                Average = gradeList.Any() ? gradeList.Average(grade => grade.Score) : 0
+            });
+
+        studentList = studentList.OrderByDescending(student => student.Average).Take(3);
+        foreach (var student in studentList)
+        {
+            Console.WriteLine(student);
+        }
     }
 }
