4c83cc47e1efaf0e7215891802a7bb720f9d58aa
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2024-07-31 13:24:03 +0330

    Result list tests.

--- phase04/FullTextSearch.Test/SearchManager/ResultList/IntersectResultListMakerTests.cs
+++ phase04/FullTextSearch.Test/SearchManager/ResultList/IntersectResultListMakerTests.cs
@@ -5,17 +5,17 @@ using phase02.SearchManager.ResultList;
 
 namespace FullTextSearch.Test.SearchManager.ResultList;
 
-public class UnionResultListMakerTests
+public class IntersectResultListMakerTests
 {
-    private readonly UnionResultListMaker _sut;
+    private readonly IntersectResultListMaker _sut;
 
-    public UnionResultListMakerTests()
+    public IntersectResultListMakerTests()
     {
-        _sut = new UnionResultListMaker();
+        _sut = new IntersectResultListMaker();
     }
 
     [Fact]
-    public void MakeResultList_ShouldReturnsUnionResultList_WhenInputDataIsOk()
+    public void MakeResultList_ShouldReturnsIntersectResultList_WhenInputDataIsOk()
     {
         //Arrange
         var keys = new List<string> { "a" };
@@ -23,40 +23,40 @@ public class UnionResultListMakerTests
         var dataMock = new Mock<ISearchable>();
         dataMock.Setup(x => x.GetKey()).Returns(keys);
         dataMock.Setup(x => x.GetValue()).Returns(docName);
-        
+
         var keys2 = new List<string> { "b" };
         var docName2 = "test2";
         var dataMock2 = new Mock<ISearchable>();
         dataMock2.Setup(x => x.GetKey()).Returns(keys2);
         dataMock2.Setup(x => x.GetValue()).Returns(docName2);
-        
+
         var keys3 = new List<string> { "a" };
         var docName3 = "test3";
         var dataMock3 = new Mock<ISearchable>();
         dataMock3.Setup(x => x.GetKey()).Returns(keys3);
         dataMock3.Setup(x => x.GetValue()).Returns(docName3);
-        
+
         var dataMap = new Dictionary<string, HashSet<ISearchable>>
         {
-            {"test", new HashSet<ISearchable>{dataMock.Object, dataMock2.Object}},
-            {"test2", new HashSet<ISearchable>{dataMock3.Object}},
-            {"test3", new HashSet<ISearchable>{}}
+            { "test", new HashSet<ISearchable> { dataMock.Object, dataMock2.Object } },
+            { "test2", new HashSet<ISearchable> { dataMock3.Object, dataMock2.Object } },
+            { "test3", new HashSet<ISearchable> { dataMock2.Object } }
         };
 
-        var expectedResult = new HashSet<ISearchable> { dataMock.Object, dataMock2.Object, dataMock3.Object };
+        var expectedResult = new HashSet<ISearchable> { dataMock2.Object };
 
         var invertedIndexMock = new Mock<IInvertedIndex>();
         invertedIndexMock.Setup(x => x.GetValue(It.IsAny<string>()))
             .Returns<string>(input => dataMap[input]);
-        
+
         //Act
-         var result= _sut.MakeResultList(dataMap.Keys.ToHashSet(), invertedIndexMock.Object);
-        
+        var result = _sut.MakeResultList(dataMap.Keys.ToHashSet(), invertedIndexMock.Object);
 
         //Assert
         Assert.Equal(result, expectedResult);
     }
-    
+
+
     [Fact]
     public void MakeResultList_ShouldReturnsEmptyResultList_WhenKeysDoesNotExistIsOk()
     {
