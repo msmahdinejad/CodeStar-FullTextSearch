d674ef49f0394b8aa72979ea74bc4a5315c47f1e
Mohammad Saleh Mahdinejad <msmahdinejad@gmail.com>
2024-08-02 00:37:40 +0330

    Search Manager Tests.

--- phase04/FullTextSearch.Test/SearchManager/SignedSearchControllerTests.cs
+++ phase04/FullTextSearch.Test/SearchManager/SignedSearchControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using phase02.Document;
+using phase02.Document.Formater;
 using phase02.InvertedIndex;
 using phase02.QueryManager.WordFinder;
 using phase02.QueryModel;
@@ -10,5 +11,34 @@ namespace FullTextSearch.Test.SearchManager;
 
 public class SignedSearchControllerTests
 {
-    
+    [Fact]
+    public void SearchWithQuery_ShouldCallCorrectMethods_Whenever()
+    {
+        //Arrange
+
+        var unSignedInputWordsMock = new Mock<IWordFinder>();
+        var positiveInputWordsMock = new Mock<IWordFinder>();
+        var negativeInputWordsMock = new Mock<IWordFinder>();
+        var intersectResultListMakerMock = new Mock<IResultListMaker>();
+        var unionResultListMakerMock = new Mock<IResultListMaker>();
+        var signedSearchStrategyMock = new Mock<ISignedSearchStrategy>();
+        var invertedIndexMock = new Mock<IInvertedIndex>();
+        var queryMock = new Mock<IQuery>();
+
+        var sut = new SignedSearchController(unSignedInputWordsMock.Object, positiveInputWordsMock.Object,
+            negativeInputWordsMock.Object, intersectResultListMakerMock.Object, unionResultListMakerMock.Object,
+            signedSearchStrategyMock.Object);
+        
+        //Act
+        sut.SearchWithQuery(queryMock.Object, invertedIndexMock.Object);
+
+        //Assert
+        unSignedInputWordsMock.Verify(x => x.FindWords(queryMock.Object.SplitedText), Times.Once);
+        positiveInputWordsMock.Verify(x => x.FindWords(queryMock.Object.SplitedText), Times.Once);
+        negativeInputWordsMock.Verify(x => x.FindWords(queryMock.Object.SplitedText), Times.Once);
+        intersectResultListMakerMock.Verify(x => x.MakeResultList(It.IsAny<HashSet<string>>(), invertedIndexMock.Object), Times.Once);
+        unionResultListMakerMock.Verify(x => x.MakeResultList(It.IsAny<HashSet<string>>(), invertedIndexMock.Object), Times.Exactly(2));
+        signedSearchStrategyMock.Verify(x =>
+            x.GetResult(It.IsAny<HashSet<ISearchable>>(), It.IsAny<HashSet<ISearchable>>(), It.IsAny<HashSet<ISearchable>>(), invertedIndexMock.Object.GetAllValue()), Times.Once);
+    }
 }
---
+++ phase04/FullTextSearch.Test/SearchManager/SignedSearchStrategyTests.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using phase02.Document;
+using phase02.Document.Formater;
+using phase02.SearchManager;
+
+namespace FullTextSearch.Test.SearchManager;
+
+public class SignedSearchStrategyTests
+{
+    private readonly SignedSearchStrategy _sut;
+
+    public SignedSearchStrategyTests()
+    {
+        _sut = new SignedSearchStrategy();
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTestData))]
+    public void GetResult_ShouldReturnExpectedResult_Whenever(
+        HashSet<ISearchable> unsignedResult,
+        HashSet<ISearchable> positiveResult,
+        HashSet<ISearchable> negativeResult,
+        HashSet<ISearchable> allResults,
+        HashSet<ISearchable> expectedResult)
+    {
+        // Act
+        var result = _sut.GetResult(unsignedResult, positiveResult, negativeResult, allResults);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetTestData()
+    {
+        var doc1 = new phase02.Document.Document("1", "", new DocumentTextEditor());
+        var doc2 = new phase02.Document.Document("2", "", new DocumentTextEditor());
+        var doc3 = new phase02.Document.Document("3", "", new DocumentTextEditor());
+        
+        yield return new object[]
+        {
+            new HashSet<ISearchable> { doc1, doc2 },
+            new HashSet<ISearchable> { doc2, doc3 },
+            new HashSet<ISearchable>(),
+            new HashSet<ISearchable> { doc1, doc2, doc3 },
+            new HashSet<ISearchable> { doc2 }
+        };
+
+        yield return new object[]
+        {
+            new HashSet<ISearchable>(),
+            new HashSet<ISearchable> { doc1, doc2 },
+            new HashSet<ISearchable> { doc2 },
+            new HashSet<ISearchable> { doc1, doc2 },
+            new HashSet<ISearchable> { doc1 }
+        };
+
+        yield return new object[]
+        {
+            new HashSet<ISearchable>(),
+            new HashSet<ISearchable>(),
+            new HashSet<ISearchable> { doc1 },
+            new HashSet<ISearchable> { doc1, doc2 },
+            new HashSet<ISearchable> { doc2 }
+        };
+
+        yield return new object[]
+        {
+            new HashSet<ISearchable>(),
+            new HashSet<ISearchable>(),
+            new HashSet<ISearchable>(),
+            new HashSet<ISearchable>(),
+            new HashSet<ISearchable>()
+        };
+    }
+}
