2b014c50b6511ee1d9bcb25c1246f2db9d9e773b
Mohammad Saleh Mahdinejad <msmahdinejad@gmail.com>
2024-08-05 17:30:33 +0330

    Completed Extractor Class.

--- phase05/Project/Document/Extractor/AdvancedDocumentWordsExtractor.cs
+++ phase05/Project/Document/Extractor/AdvancedDocumentWordsExtractor.cs
@@ -4,6 +4,21 @@ public class AdvancedDocumentWordsExtractor : IExtractor
 {
     public IEnumerable<string> GetKey(ISearchable data)
     {
-        return data.GetWords();
+        var words = data.GetWords();
+        var result = new List<string>();
+        for (int i = 1; i <= 4; i++)
+        {
+            for (int j = 0; j <= words.Count() - i; j++)
+            {
+                string sentence = words.ElementAt(j);
+                for (int k = 1; k < i; k++)
+                {
+                    sentence = sentence + " " + words.ElementAt(j+k);
+                }
+                result.Add(sentence);
+            }
+        }
+
+        return result;
     }
 }
