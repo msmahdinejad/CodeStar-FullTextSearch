718a75644f8a729631e4b9d675d4ed446f32c9cb
Mohammad Saleh Mahdinejad <msmahdinejad@gmail.com>
2024-08-05 14:50:09 +0330

    added AdvancedQuery Tests.

---
+++ phase05/Project/QueryModel/AdvancedQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace phase02.QueryModel;
+
+public class AdvancedQuery : IQuery
+{
+    public string Text { get; init; }
+    public string[] SplitedText => Text.ToUpper().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    public AdvancedQuery(string text) => Text = text;
+    
+}
